f4d793a9df7e03b4281dfa9fa3d408bee4a3a9b3
Cheryl Casteling <cheryl.casteling@unlimtedgroup.com>
2019-06-28 14:00:31 +0100

    Added chargefine method to Library.js

--- Library.js
+++ Library.js
@@ -2,6 +2,7 @@ class Library {
   constructor(){
     this.books = [];
     this.patrons = [];
+    this.dailyFine = .1;
   }
 
   addBook(book) {
@@ -10,5 +11,18 @@ class Library {
   
   addPatron(patron) {
     this.patrons.push(patron);
+  }
+  chargeFines() {
+    const now = new Date();
+
+    const latePatrons = this.patrons.filter(patron => 
+      (patron.currentBook !== null && patron.currentBook.dueDate < now)
+    );
+
+    for (let patron of latePatrons) {
+      const dateDiff = new Date(now - patron.currentBook.dueDate);
+      const daysLate = dateDiff.getDate();
+      patron.balance += this.dailyFine * daysLate;
+    }
   }
 }
