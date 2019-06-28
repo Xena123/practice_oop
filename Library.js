07b0c9fe0168a633b498ace40151a1217f1959d6
Cheryl Casteling <cheryl.casteling@unlimtedgroup.com>
2019-06-28 13:15:03 +0100

    Initial commit

---
+++ Library.js
@@ -0,0 +1,14 @@
+class Library {
+  constructor(){
+    this.books = [];
+    this.patrons = [];
+  }
+
+  addBook(book) {
+    this.books.push(book);
+  }
+  
+  addPatron(patron) {
+    this.patrons.push(patron);
+  }
+}
---
+++ Patron.js
@@ -0,0 +1,17 @@
+class Patron {
+  constructor(name, email){
+    this.name = name;
+    this.email = email;
+    this.currentBook = null;
+  }
+  checkOut(book) {
+    this.currentBook = book;
+    book.out = true;
+    book.patron = this;
+  }
+  returnBook(book) {
+    this.currentBook = null;
+    book.out = false;
+    book.patron = null;
+  }
+}
